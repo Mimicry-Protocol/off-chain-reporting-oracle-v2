37648e9175219da97e1443a0a4d6a260b8453740
Aslan Goldenhour <aslan@mimicry.finance>
2023-05-13 03:24:16 -0500

    Updated license

--- contracts/OpenMarketsOracle.sol
+++ contracts/OpenMarketsOracle.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPLv3
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";

0fffc52d7c25546a804f80402ab1bdc8eb86a625
Aslan Goldenhour <aslan@mimicry.finance>
2023-05-14 13:21:11 -0500

    initial commits of the cloner

---
+++ web3-functions/open-markets-oracle-cloner/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Open-Markets-Oracle Cloner
+ * 
+ * This function writes the latest value of an OMO on one chain
+ * to another chain. This is particularlly useful when wanting to 
+ * mirror oracles between mainnet and testnet.
+ * 
+ * Currently only clones from Polygon's OMO to Mumbai's OMO
+ */
